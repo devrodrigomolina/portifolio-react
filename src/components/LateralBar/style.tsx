0c0e46f45325748b220fad3c82e55374b01a8d57
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-01 23:32:56 -0300

    :lipstick: feat: Estilização da barra lateral

---
+++ src/components/LateralBar/style.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const ContainerLateralBar = styled.div`
+  width: 2px;
+  height: 100%;
+  background: #a3a5a7;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  .circle {
+    width: 30px;
+    height: 30px;
+    border-radius: 50px;
+    border: 2px solid #a3a5a7;
+    background-color: #1D222A;
+    display: flex;
+    box-shadow: 7px 7px 1px #a3a5a730;
+    z-index: 10;
+    
+  }
+  .image-pat {
+    width: 200px;
+    position: absolute;
+    bottom: -150px;
+    right: -50px;
+    z-index: 9;
+  }
+`
