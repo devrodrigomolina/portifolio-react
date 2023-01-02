3974203459c8be65eb4b546607f12651413dcd03
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-01 23:33:04 -0300

    :lipstick: feat: Estilização do header

---
+++ src/components/Header/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  width: 100%;
+  height: 90px;
+  padding: 30px;
+  display: flex;
+  justify-content: space-between;
+`
+export const Logo = styled.div`
+  color: white;
+  cursor: pointer;
+  width: 350px;
+  span {
+    color: #29A587;
+  }
+`
+
+export const MenuItens = styled.div`
+
+`
