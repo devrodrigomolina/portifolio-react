2befb4d2442dfdfc11fbe8a327dca3885f953117
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-06 00:23:26 -0300

    :lipstick: feat: Alterado cor do background

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,7 +1,7 @@
 import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
- * {
+ *, ::after, ::before {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
@@ -13,9 +13,14 @@ export const GlobalStyles = createGlobalStyle`
   ul {
     list-style: none;
   }
+  html {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
   body {
     background-color: #222831;
     font-family: 'Jost', sans-serif;
+    background-color: transparent;
+  background-image: linear-gradient(0deg, #31363c 0%, #222831 100%);
   }
   img {
     width: 100%;
