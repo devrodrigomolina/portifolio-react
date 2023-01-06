f63b32301793ab893a0878531921bb1d626b07f6
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-06 00:22:36 -0300

    :sparkles: feat: Componente de titulos

---
+++ src/components/Titles/Titles.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import * as C from './style'
+
+interface TITLEPROPS {
+  title: string,
+  subtitle: string,
+  secondSubtitle: string
+}
+
+const Titles = ({ title, subtitle, secondSubtitle }: TITLEPROPS) => {
+  return (
+  <C.ContainerTitle>
+    <h2 className='title'>{title}</h2>
+    <div className="container-subtitle">
+      <span className='subtitle'>{subtitle} <span className='second-subtitle'>{secondSubtitle}</span></span>
+    </div>
+  </C.ContainerTitle>
+  )
+}
+
+export default Titles
