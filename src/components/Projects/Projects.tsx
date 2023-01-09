f8aeb6328f10ed4a683d8b2d3562724405f47fef
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-09 00:14:13 -0300

    :sparkles: feat: Componente de projetos

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Cards from "../Cards/Cards";
+import Titles from "../Titles/Titles";
+import * as C from "./style";
+import { ProjectsJson } from "./ProjectsJson";
+import LateralBar from "../LateralBar/LateralBar";
+
+const Projects = () => {
+  return (
+    <C.ContainerProjects>
+      <Titles title="PORTIFÓLIO" subtitle="MEUS" secondSubtitle="PROJETOS" />
+      <div className="cards-container">
+        {ProjectsJson.map((project) => (
+          <Cards
+          image={project.image}
+          desc={project.desc}
+          projectType={project.projectType}
+          projectName={project.projectname}
+          height={570}
+          />
+          ))}
+      </div>
+      <div className="bar-lateral">
+        <LateralBar />
+      </div>
+      <h1 className="bg-text">Portifólio</h1>
+    </C.ContainerProjects>
+  );
+};
+
+export default Projects;
