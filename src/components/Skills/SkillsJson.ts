8f13afaa41db36608ff511c02b20eeb8c6e3aec6
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-09 00:14:02 -0300

    :sparkles: feat: Criado JSON das skills

---
+++ src/components/Skills/SkillsJson.ts
@@ -0,0 +1,33 @@
+export const SkillsJson = [
+  {
+    tech: 'HTML',
+    value: 90,
+    description: 'HTML é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores. A tecnologia é fruto da junção entre os padrões HyTime e SGML. HyTime é um padrão para a representação estruturada de hipermídia e conteúdo baseado em tempo'
+  },
+  {
+    tech: 'CSS',
+    value: 80,
+    description: 'Cascading Style Sheets é um mecanismo para adicionar estilos a uma página web, aplicado diretamente nas tags HTML ou ficar contido dentro das tags <style>. Também é possível, adicionar estilos adicionando um link para um arquivo CSS que contém os estilos'
+  },
+  {
+    tech: 'JAVASCRIPT',
+    value: 70,
+    description: 'JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.'
+  },
+  {
+    tech: 'TYPESCRIPT',
+    value: 60,
+    description: 'TypeScript é uma linguagem de programação de código aberto desenvolvida pela Microsoft. É um superconjunto sintático estrito de JavaScript e adiciona tipagem estática opcional à linguagem.'
+  },
+  {
+    tech: 'VUE.JS',
+    value: 70,
+    description: 'Vue.js é um framework JavaScript de código-aberto, focado no desenvolvimento de interfaces de usuário e aplicativos de página única.'
+  },
+  {
+    tech: 'REACT.JS',
+    value: 85,
+    description: 'O React é uma biblioteca front-end JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.'
+  },
+
+]
