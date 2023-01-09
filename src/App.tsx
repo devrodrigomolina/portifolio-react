0caaa345d0d0654858b5a6023f8274f627179b27
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-09 00:08:09 -0300

    :recycle: Importação dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@
 import About from "./components/AboutSection/About";
 import Header from "./components/Header/Header";
 import MyServices from "./components/MyServices/MyServices";
+import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
 import { GlobalStyles } from "./styles/GlobalStyles";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
         <Header />
         <About />
         <MyServices />
+        <Skills />
+        <Projects />
         <GlobalStyles />
       </>
 
