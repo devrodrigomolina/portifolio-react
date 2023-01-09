00b4141df4e3e6385d15cf1bee6bf9d4314ee606
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-09 00:14:23 -0300

    :sparkles: feat: JSON dos projetos

---
+++ src/components/Projects/ProjectsJson.ts
@@ -0,0 +1,29 @@
+import tasksbook from '../../assets/tasksbook.png'
+import petshop from '../../assets/petshop.gif'
+export const ProjectsJson = [
+  {
+    projectname: 'TasksBook',
+    projectType: 'Projeto pessoal',
+    desc: 'Aplicação SPA feita com Vue.js/Nuxt.js para o gerenciamento de tarefas. O sistema engloba um painel para a visualização geral das tarefas e com gráficos dinâmicos que mudam conforme a atualização do status da tarefa e layout 100% responsivo feito com css puro.',
+    image: tasksbook
+  },
+  {
+    projectname: 'Wooof PetShop',
+    projectType: 'Projeto pessoal',
+    desc: 'Este projeto é um marketplace voltado a petshop, Fornecemos linhas de produtos especializados, tais como comida orgânica, ração natural para e uma seleção ampla de guloseimas para cães e gatos.',
+    image: petshop
+  },
+  {
+    projectname: 'sadasdsaasd',
+    projectType: 'Projeto pessoal',
+    desc: 'awewqewqe',
+    image: tasksbook
+  },
+  {
+    projectname: 'sadasdsaasd',
+    projectType: 'Projeto pessoal',
+    desc: 'awewqewqe',
+    image: tasksbook
+  },
+
+]
