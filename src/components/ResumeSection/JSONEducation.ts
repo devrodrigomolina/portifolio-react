010d9eff5e7d180ec371cc5157d0d1b3370e4fac
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-16 00:14:31 -0300

    :sparkles: Criado arquivo JSON

---
+++ src/components/ResumeSection/JSONEducation.ts
@@ -0,0 +1,18 @@
+
+export const JSONEducation = [
+ {
+  name: 'UNICV - Centro Universitário Cidade Verde',
+  course: 'Analise e Desenvolvimento de Sistemas',
+  initialDate: '2021',
+  endDate: 'PRESENT',
+  desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nisi, odio atque earum in omnis minus commodi temporibus animi eligendi voluptates! Necessitatibus quisquam explicabo quod deserunt? Atque voluptas laudantium molestiae.'
+ },
+ {
+  name: 'B7WEB',
+  course: 'React.JS',
+  initialDate: '2021',
+  endDate: 'PRESENT',
+  desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nisi, odio atque earum in omnis minus commodi temporibus animi eligendi voluptates! Necessitatibus quisquam explicabo quod deserunt? Atque voluptas laudantium molestiae.'
+ }
+
+]
