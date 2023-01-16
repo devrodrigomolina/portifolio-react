ac69f1716d3aada665101b40d3e59f1fea262a08
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-16 00:14:52 -0300

    :recycle: Feito map no JSON

--- src/components/ResumeSection/Resume.tsx
+++ src/components/ResumeSection/Resume.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Titles from '../Titles/Titles'
 import * as C from './style'
+import { JSONEducation } from './JSONEducation'
 
 const Resume = () => {
   const [showItems, setShowItems] = useState<boolean>(false)
@@ -10,21 +11,24 @@ const Resume = () => {
       <Titles title='RESUMO' subtitle='MINHA' secondSubtitle='Historia' />
       <C.ContainerHistory>
         <C.LeftSide showItems={showItems}>
+          
           <h1 className='titles'>Educação</h1>
+          {JSONEducation.map(elements => (
+         
           <div className="courses">
             <div className="container-instituition"  onClick={() => setShowItems(prevState => !prevState)}>
-              <span className='name-instituition'>UNICV - Centro Universitário Cidade Verde</span>
+              <span className='name-instituition'>{elements.name}</span>
             </div>
 
             <div className="itens-courses">
-                <div className="category-date-course">
-                  <span className='name-course'>Analise e Desenvolvimento de Sistemas</span>
-                  <span className='date'>2021 - <span className='date-end'>PRESENT</span></span>
-                </div>
-                <p className='desc-course'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nisi, odio atque earum in omnis minus commodi temporibus animi eligendi voluptates! Necessitatibus quisquam explicabo quod deserunt? Atque voluptas laudantium molestiae.
-                </p>
+              <div className="category-date-course">
+                <span className='name-course'>{elements.course}</span>
+                <span className='date'>{elements.initialDate} - <span className='date-end'>{elements.endDate}</span></span>
+              </div>
+              <p className='desc-course'>{elements.desc}</p>
             </div>
           </div>
+          )) }
         </C.LeftSide>
 
         <C.RightSide>
