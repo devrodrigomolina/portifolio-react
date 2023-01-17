8f17d0a7163ce4fec266a0076e909773eac5a1c0
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-16 23:23:53 -0300

    :lipstick: Adicionado estilos dos textos do background

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -25,4 +25,15 @@ export const GlobalStyles = createGlobalStyle`
     width: 100%;
     display: block;
   }
+  .bg-text {
+    font-size: 18vw;
+    position: relative;
+    bottom: 220px;
+    color: rgba(255, 255, 255, 0.02);
+    text-align: center;
+    font-weight: bold;
+    font-family: "Caveat", cursive;
+    z-index: 0;
+    cursor: default;
+  }
 `
