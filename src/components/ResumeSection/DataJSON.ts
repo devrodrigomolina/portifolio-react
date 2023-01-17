0f66c183f9f04a292809f12ffe4dead6e32ed19b
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-16 23:26:56 -0300

    :dizzy: feat: Criado JSON de cursos e experiencia

---
+++ src/components/ResumeSection/DataJSON.ts
@@ -0,0 +1,55 @@
+export const JSONEducation = [
+  {
+    id: 1,
+    name: "UNICV - Centro Universitário Cidade Verde",
+    course: "Analise e Desenvolvimento de Sistemas",
+    initialDate: "2021",
+    endDate: "PRESENT",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nisi, odio atque earum in omnis minus commodi temporibus animi eligendi voluptates! Necessitatibus quisquam explicabo quod deserunt? Atque voluptas laudantium molestiae.",
+  },
+  {
+    id: 2,
+    name: "B7WEB",
+    course: "React.JS",
+    initialDate: "2021",
+    endDate: "PRESENT",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nisi, odio atque earum in omnis minus commodi temporibus animi eligendi voluptates! Necessitatibus quisquam explicabo quod deserunt? Atque voluptas laudantium molestiae.",
+  },
+  {
+    id: 3,
+    name: "B7WEB",
+    course: "React.JS",
+    initialDate: "2021",
+    endDate: "PRESENT",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nisi, odio atque earum in omnis minus commodi temporibus animi eligendi voluptates! Necessitatibus quisquam explicabo quod deserunt? Atque voluptas laudantium molestiae.",
+  },
+];
+
+export const JSONExperience = [
+  {
+    id: 1,
+    name: "Guiador Digital",
+    course: "Analise e Desenvolvimento de Sistemas",
+    initialDate: "2021",
+    endDate: "PRESENT",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nisi, odio atque earum in omnis minus commodi temporibus animi eligendi voluptates! Necessitatibus quisquam explicabo quod deserunt? Atque voluptas laudantium molestiae.",
+  },
+  {
+    id: 2,
+    name: "Guiador Digital",
+    course: "Analise e Desenvolvimento de Sistemas",
+    initialDate: "2021",
+    endDate: "PRESENT",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nisi, odio atque earum in omnis minus commodi temporibus animi eligendi voluptates! Necessitatibus quisquam explicabo quod deserunt? Atque voluptas laudantium molestiae.",
+  },
+  {
+    id: 3,
+    name: "Guiador Digital",
+    course: "Analise e Desenvolvimento de Sistemas",
+    initialDate: "2021",
+    endDate: "PRESENT",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nisi, odio atque earum in omnis minus commodi temporibus animi eligendi voluptates! Necessitatibus quisquam explicabo quod deserunt? Atque voluptas laudantium molestiae.",
+  },
+
+
+];
