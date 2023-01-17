a0ba21b8f70aebcc96da3a577cbcd24c492a46c8
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-16 23:27:34 -0300

    :lipstick: Reestruturação dos estilos

--- src/components/ResumeSection/style.ts
+++ src/components/ResumeSection/style.ts
@@ -1,15 +1,13 @@
 import styled from "styled-components";
 import { devices } from "../../styles/Responsive";
 
-type ShowItemsType = {
-  showItems: boolean
-}
+
 
 export const ContainerResume = styled.div`
   background-color: transparent;
   background-image: linear-gradient(180deg, #31363c 0%, #222831 100%);
   margin-bottom: 300px;
-  .titles {
+ .titles {
     font-size: 1.8rem;
     text-align: center;
     color: white;
@@ -20,87 +18,41 @@ export const ContainerResume = styled.div`
 
 export const ContainerHistory = styled.div`
   display: flex;
-  width: 80vw;
+  width: 70vw;
   margin: 0 auto;
+  position: relative;
+  @media ${devices.laptop} {
+    width: 90vw;
+    align-items: center;
+  }
+  .bar {
+    position: absolute;
+    height: calc(100% + 150px);
+    left: -100px;
+  } 
   @media ${devices.laptop} {
     flex-direction: column;
   }
 `
 
-export const LeftSide = styled.div<ShowItemsType>`
+export const LeftSide = styled.div`
   width: 50%;
-  margin-right: 15px;
+  margin-right: 45px;
   position: relative;
   @media ${devices.laptop} {
+    width: 90%;
     margin-bottom: 130px;
+    margin-right: 0;
   }
-  .courses {
-    border-bottom: 2px solid #a3a5a7;
-    color: white;
-    max-height: ${props => (props.showItems ? "500px" : "100px")};
-    transition: all 1s;
-    overflow: hidden;
-    .container-instituition {
-      width: 100%;
-      padding: 40px;
-      &::after {
-        content: '${props => props.showItems ? '-' : '+'}';
-        position: absolute;
-        right: -24px;
-        
-        bottom: -24px;
-        width: 48px;
-        height: 48px;
-        background: #1d222a;
-        line-height: 40px;
-      border-radius: 50%;
-      border: 2px solid #a3a5a7;
-      box-shadow: 5px 5px rgb(0 0 0 / 20%);
-      font-weight: 400;
-      font-size: 30px;
-      text-align: center;
-      transition: all 1.2s cubic-bezier(0.3, 0, 0.3, 1);
-      -webkit-transition: all 1.2s cubic-bezier(0.3, 0, 0.3, 1);
-      z-index: 200;
-    }
-    .name-instituition {
-      font-size: 1.2rem;
-      font-weight: 700;
-      cursor: pointer;
-    }
-  }
-  
-  .itens-courses {
-    
-    padding: 25px;
-    transition: all 3s;
-    
-    .category-date-course {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      .name-course {
-        font-family: "Caveat", cursive;
-        font-weight: bold;
-        font-size: 1.7rem;
-      }
-      .date {
-        font-weight: bold;
-        .date-end {
-          color: #29a587;
-        }
-      }
-    }
-    
-    .desc-course {
-      opacity: 0.5;
-      margin-top: 20px;
-    }
-    
-  }
-}
 `
+
+
+
 export const RightSide = styled.div`
   width: 50%;
-  margin-left: 15px;
+  margin-left: 45px;
+  @media ${devices.laptop} {
+    margin-left: 0;
+    width: 90%
+  }
   `
