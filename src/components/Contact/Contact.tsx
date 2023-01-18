9f98dec1f53ad9cfbbb2f587eb9dc307d7fa9fea
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-18 00:06:14 -0300

    :sparkles: feat: Criado componente de contato

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Titles from '../Titles/Titles'
+import * as C from './style'
+const Contact = () => {
+  return (
+    <C.ContainerContact>
+      <Titles title='ENTRE EM CONTATO' subtitle="VAMOS" secondSubtitle='Falar de Ideias' />
+    </C.ContainerContact>
+  )
+}
+
+export default Contact
