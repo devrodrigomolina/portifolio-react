75c60a4413f7425bc88fdf450d3fe46559c1b1b5
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-18 00:04:16 -0300

    :white_check_mark:	test: Criado JSON provisorio para testar os cards

---
+++ src/components/Testimonials/Services.ts
@@ -0,0 +1,25 @@
+
+
+export const ServicesJson = [
+  {
+    title: 'DESENVOLVEDOR WEB',
+    subtitle: 'Sites & aplicações',
+    desc: ' Atuo no desenvolvimento de aplicações performática e responsiva com as melhores tecnologias do mercado',
+  },
+  {
+    title: 'DESENVOLVEDOR WEB',
+    subtitle: 'Sites & aplicações',
+    desc: ' Atuo no desenvolvimento de aplicações performática e responsiva com as melhores tecnologias do mercado'
+  },
+  {
+    title: 'DESENVOLVEDOR WEB',
+    subtitle: 'Sites & aplicações',
+    desc: ' Atuo no desenvolvimento de aplicações performática e responsiva com as melhores tecnologias do mercado'
+  },
+  {
+    title: 'DESENVOLVEDOR WEB',
+    subtitle: 'Sites & aplicações',
+    desc: ' Atuo no desenvolvimento de aplicações performática e responsiva com as melhores tecnologias do mercado'
+  },
+
+]
