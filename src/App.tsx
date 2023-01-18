901b10cbd136faafc0b1f314214e84927ebe8245
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-18 00:01:09 -0300

    :sparkles: Importado componente testimonials

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import About from "./components/AboutSection/About";
+import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
 import MyServices from "./components/MyServices/MyServices";
 import Projects from "./components/Projects/Projects";
 import Resume from "./components/ResumeSection/Resume";
 import Skills from "./components/Skills/Skills";
+import Testimonials from "./components/Testimonials/Testimonials";
 import { GlobalStyles } from "./styles/GlobalStyles";
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
         <Skills />
         <Projects />
         <Resume />
+        <Testimonials />
+        {/* <Contact /> */}
         <GlobalStyles />
       </>
 
