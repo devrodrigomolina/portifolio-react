98e2b743c9d1b6eaa34fccb6b9b79d44eef36443
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-21 04:06:03 -0300

    :dizzy: feat: Adicionado animação e alterado imagem

--- src/components/AboutSection/About.tsx
+++ src/components/AboutSection/About.tsx
@@ -1,54 +1,95 @@
-import React from 'react'
-import LateralBar from '../LateralBar/LateralBar'
-import * as C from './style'
-import { BsLinkedin } from 'react-icons/bs'
-import { AiFillGithub } from 'react-icons/ai'
-import { AiOutlineWhatsApp } from 'react-icons/ai'
-import myphoto from '../../assets/minhafoto2.png'
-import pat1 from '../../assets/pat1.png'
-import pat2 from '../../assets/pat2.png'
-import Socias from '../Sociais/Socias'
+import LateralBar from "../LateralBar/LateralBar";
+import * as C from "./style";
+import myphoto from "../../assets/minhafoto2.png";
+import pat1 from "../../assets/pat1.png";
+import pat2 from "../../assets/pat2.png";
+import Socias from "../Sociais/Socias";
+import { Link } from "react-scroll";
+import BoxAnimation from "../../hooks/useAnimationsScroll";
 
 const About = () => {
   return (
-    <C.AboutSection id='home'>
-      <C.AboutContainer>
-        <C.LeftSide>
-          <LateralBar />
-          <div className="infos">
+    <C.AboutSection id="home">
+      <BoxAnimation>
+        <C.AboutContainer>
+          <C.LeftSide>
+            <LateralBar />
+            <div className="infos">
+              <h3 className="hello">
+                OLÁ, <span>MEU NOME É</span>
+              </h3>
+              <h1 className="name">
+                <span>RODRIGO</span> MOLINA
+              </h1>
+              <h3 className="iam">
+                EU SOU<span className="webdev">Desenvolvedor Front end</span>
+              </h3>
 
-          <h3 className='hello'>OLÁ, <span>MEU NOME É</span></h3>
-          <h1 className='name'><span>RODRIGO</span> MOLINA</h1>
-          <h3 className='iam'>EU SOU<span className='webdev'>Desenvolvedor Front end</span></h3>
+              <p className="aboutme">
+                Possuo uma vasta experiencia com Desenvolvimento front end, com
+                as principais tecnologias do mercado.
+              </p>
 
-          <p className='aboutme'>Possuo uma vasta experiencia com Desenvolvimento front end, com as principais tecnologias do mercado.</p>
+              <div className="sociais">
+                <Socias />
+              </div>
 
-          <div className="sociais">
-            <Socias />
-          </div>
-          
-          <div className="btns">
-            <a href=""  target="_blank" className='btn'>
-              <span>DOWNLOAD CV</span>
-            </a>
-            <a href="" className='skills'>SKILLS</a>
-          </div>
-          </div>
-        </C.LeftSide>
+              <div className="btns">
+                <a
+                  href="../../assets/cv.pdf"
+                  download
+                  target="_blank"
+                  className="btn"
+                >
+                  <span>DOWNLOAD CV</span>
+                </a>
+                <Link
+                  to="skills"
+                  className="skills"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={1000}
+                >
+                  SKILLS
+                </Link>
+              </div>
+            </div>
+          </C.LeftSide>
 
-        <C.RigthSide>
-          <div className="container-photo">
-            <img className='image-pat1' src={pat1} alt="" />
-            <img className='myphoto' src={myphoto} alt="" />
-            <img className='image-pat2' src={pat1} alt="" />
-            <img className='image-pat3' src={pat2} alt="" />
-          </div>
-  
-        </C.RigthSide>
-      </C.AboutContainer>
-      <h1 className='bg-text'>Desenvolvedor</h1>
+          <C.RigthSide>
+            <div className="container-photo">
+              <img className="image-pat1" src={pat1} alt="" />
+              <img className="myphoto" src={myphoto} alt="" />
+              <img className="image-pat2" src={pat1} alt="" />
+              <img className="image-pat3" src={pat2} alt="" />
+              <div className="infos-git">
+                <ul className="git-stats">
+                  <li className="stats">
+                    <span className="num">
+                      32
+                    </span>
+                    <span className="value">
+                      <span>PROJETOS</span> COMPLETOS
+                    </span>
+                  </li>
+                  <li className="stats">  
+                    <span className="num">
+                      835
+                    </span>
+                    <span className="value">
+                      COMMITS
+                    </span>                  
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </C.RigthSide>
+        </C.AboutContainer>
+        <h1 className="bg-text">Desenvolvedor</h1>
+      </BoxAnimation>
     </C.AboutSection>
-  )
-}
+  );
+};
 
-export default About
+export default About;
