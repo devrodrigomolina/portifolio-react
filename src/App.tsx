79e0935eba698f070dd7e61742e236178171c9b9
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-21 04:02:49 -0300

    :dizzy: feat: Adicionado animação ao abrir site

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 
+import { useEffect } from "react";
 import About from "./components/AboutSection/About";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
@@ -9,10 +10,18 @@ import Resume from "./components/ResumeSection/Resume";
 import Skills from "./components/Skills/Skills";
 import Testimonials from "./components/Testimonials/Testimonials";
 import { GlobalStyles } from "./styles/GlobalStyles";
+import { motion } from "framer-motion"
 
 function App() {
+
+  const variants = {
+    open: { opacity: 1, x: 0 },
+    closed: { opacity: 0, x: "-100%" },
+  }
+ 
   return (
-      <>
+      <motion.div  initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}>
         <Header />
         <About />
         <MyServices />
@@ -23,7 +32,7 @@ function App() {
         <Contact />
         <Footer />
         <GlobalStyles />
-      </>
+      </motion.div>
 
   );
 }
