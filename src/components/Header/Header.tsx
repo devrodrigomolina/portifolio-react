c34b2eecac40553339c693fbe354656c29ce6560
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-21 04:06:24 -0300

    :recycle: refactor: Alterado de h2 para Link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import Menu from './Menu/Menu'
-import * as C from './style'
+import React from "react";
+import { Link } from "react-scroll";
+import Menu from "./Menu/Menu";
+import * as C from "./style";
 
 const Header = () => {
   return (
     <C.Header>
       <C.Logo>
-        <h2><span>&#60;</span> DEV RODRIGO MOLINA <span>/ &#62;</span></h2>
+        <Link to="home" spy={true} smooth={true} offset={-200} duration={1000}>
+          <span>&#60;</span> DEV RODRIGO MOLINA <span>/ &#62;</span>
+        </Link>
       </C.Logo>
       <C.MenuItens>
         <Menu />
       </C.MenuItens>
     </C.Header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
