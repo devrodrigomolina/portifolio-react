f790383bfe791d850196dafee8e95b951a185ea8
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2023-01-23 00:14:10 -0300

    :recycle: refactor: Alterado textos

--- src/components/MyServices/Services.ts
+++ src/components/MyServices/Services.ts
@@ -2,22 +2,26 @@ export const ServicesJson = [
   {
     title: 'DESENVOLVEDOR WEB',
     subtitle: 'Sites',
-    desc: 'Atuo no desenvolvimento de sites performáticos e responsivos com as melhores tecnologias do mercado',
+    desc: 'Atuo no desenvolvimento de sites performáticos e responsivos, com as melhores tecnologias do mercado',
+    link: 'https://pt.wikipedia.org/wiki/Desenvolvimento_web'
   },
   {
     title: 'DESENVOLVEDOR DE SOFTWARE',
     subtitle: 'Aplicações',
-    desc: 'Desenvolvo aplicações performáticas visando criar soluções para atender problemas de pequenas e médias empresas'
+    desc: 'Desenvolvo aplicações performáticas, visando criar soluções para atender problemas de pequenas e médias empresas',
+    link: 'https://pt.wikipedia.org/wiki/Desenvolvimento_de_software'
   },
   {
     title: 'DESENVOLVEDOR WEB',
-    subtitle: 'Sites & aplicações',
-    desc: ' Atuo no desenvolvimento de aplicações performática e responsiva com as melhores tecnologias do mercado'
+    subtitle: 'Landing pages',
+    desc: 'Atuo no desenvolvimento de landing pages, visando colocar o seu negócio no universo digital!',
+    link: 'https://pt.wikipedia.org/wiki/Landing_page'
   },
   {
     title: 'DESENVOLVEDOR WEB',
-    subtitle: 'Sites & aplicações',
-    desc: ' Atuo no desenvolvimento de aplicações performática e responsiva com as melhores tecnologias do mercado'
+    subtitle: 'Painéis administrativos',
+    desc: 'Atuo no desenvolvimento de Painéis administrativos de variados estilos, de acordo com o que o cliente precisa.',
+    link: ''
   },
 
 ]
