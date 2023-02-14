4e4e243fdeade7b19616e5ad4a5c84309e8cd3f6
Rodrigo Molina <75998678+devrodrigomolina@users.noreply.github.com>
2023-02-13 21:33:02 -0300

    feat: Adicionado ID's

--- src/components/MyServices/Services.ts
+++ src/components/MyServices/Services.ts
@@ -1,23 +1,27 @@
 export const ServicesJson = [
   {
+    id: 1,
     title: 'DESENVOLVEDOR WEB',
     subtitle: 'Sites',
     desc: 'Atuo no desenvolvimento de sites performáticos e responsivos, com as melhores tecnologias do mercado',
     link: 'https://pt.wikipedia.org/wiki/Desenvolvimento_web'
   },
   {
+    id: 2,
     title: 'DESENVOLVEDOR DE SOFTWARE',
     subtitle: 'Aplicações',
     desc: 'Desenvolvo aplicações performáticas, visando criar soluções para atender problemas de pequenas e médias empresas',
     link: 'https://pt.wikipedia.org/wiki/Desenvolvimento_de_software'
   },
   {
+    id: 3,
     title: 'DESENVOLVEDOR WEB',
     subtitle: 'Landing pages',
     desc: 'Atuo no desenvolvimento de landing pages, visando colocar o seu negócio no universo digital!',
     link: 'https://pt.wikipedia.org/wiki/Landing_page'
   },
   {
+    id: 4,
     title: 'DESENVOLVEDOR WEB',
     subtitle: 'Painéis administrativos',
     desc: 'Atuo no desenvolvimento de Painéis administrativos de variados estilos, de acordo com o que o cliente precisa.',
--- src/components/Skills/SkillsJson.ts
+++ src/components/Skills/SkillsJson.ts
@@ -1,32 +1,38 @@
 export const SkillsJson = [
   {
+    id: 1,
     tech: 'HTML',
     value: 90,
     description: 'HTML é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores. A tecnologia é fruto da junção entre os padrões HyTime e SGML. HyTime é um padrão para a representação estruturada de hipermídia e conteúdo baseado em tempo'
   },
   {
+    id: 2,
     tech: 'CSS',
     value: 80,
     description: 'Cascading Style Sheets é um mecanismo para adicionar estilos a uma página web, aplicado diretamente nas tags HTML ou ficar contido dentro das tags <style>. Também é possível, adicionar estilos adicionando um link para um arquivo CSS que contém os estilos'
   },
   {
+    id: 3,
     tech: 'JAVASCRIPT',
     value: 70,
     description: 'JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.'
   },
   {
+    id: 4,
     tech: 'TYPESCRIPT',
     value: 60,
     description: 'TypeScript é uma linguagem de programação de código aberto desenvolvida pela Microsoft. É um superconjunto sintático estrito de JavaScript e adiciona tipagem estática opcional à linguagem.'
   },
   {
+    id: 5,
     tech: 'VUE.JS',
     value: 70,
     description: 'Vue.js é um framework JavaScript de código-aberto, focado no desenvolvimento de interfaces de usuário e aplicativos de página única.'
   },
   {
+    id: 6,
     tech: 'REACT.JS',
-    value: 85,
+    value: 70,
     description: 'O React é uma biblioteca front-end JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.'
   },
 
