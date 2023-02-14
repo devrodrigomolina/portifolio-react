7255b10b70c86b8d46441a26468d6814953b0c6a
Rodrigo Molina <75998678+devrodrigomolina@users.noreply.github.com>
2023-02-13 21:30:24 -0300

    fix: Corrigido bug que acontecia quando o card tinha outro estilo

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,5 +1,6 @@
 import * as C from "./style";
 import { FaArrowRight } from "react-icons/fa";
+import { RiDoubleQuotesR } from "react-icons/ri";
 import { CardsType } from "../../types/CardsTypes";
 import BoxAnimation from "../../hooks/useAnimationsScroll";
 
@@ -12,33 +13,58 @@ const Cards = ({
   desc,
   height,
   arrow_text,
-  link
+  link,
+  type,
 }: CardsType) => {
   return (
     <BoxAnimation>
       <C.ContainerCards height={height}>
-        <div className="infos-card">
-          {image && (
-            <div className="container-image">
-              <img className="image" src={image} />
-            </div>
-          )}
-          {projectType && <h3 className="project-type">{projectType}</h3>}
-          {projectName && <h2 className="project-name">{projectName}</h2>}
-          {title && subtitle && (
-            <>
-              <h3 className="title">{title}</h3>
-              <h1 className="subtitle">{subtitle}</h1>
-            </>
-          )}
-          <p className="desc">{desc}</p>
-         
-          <a href={link} className="arrow" target="_blank">
-            {arrow_text} <FaArrowRight className="icon" />
-          </a>
-        
-          <div className="image-pat" />
-        </div>
+        {type !== "card-comments" ? (
+          <div className="infos-card">
+            {image && (
+              <div className="container-image">
+                <img className="image" src={image} />
+              </div>
+            )}
+            {projectType && <h3 className="project-type">{projectType}</h3>}
+            {projectName && <h2 className="project-name">{projectName}</h2>}
+            {title && subtitle && (
+              <>
+                <h3 className="title">{title}</h3>
+                <h1 className="subtitle">{subtitle}</h1>
+              </>
+            )}
+            <p className="desc">{desc}</p>
+
+            <a href={link} className="arrow" target="_blank">
+              {arrow_text} <FaArrowRight className="icon" />
+            </a>
+
+            <div className="image-pat" />
+          </div>
+        ) : (
+          <C.CardComments height={height}>
+            {image && (
+              <div className="comment-image">
+                <div className="icon-comment">
+                  <RiDoubleQuotesR className="icon" />
+                </div>
+                <img className="image" src={image} />
+              </div>
+            )}
+            <p className="desc">{desc}</p>
+            {projectType && <h3 className="project-type">{projectType}</h3>}
+            {projectName && <h2 className="project-name">{projectName}</h2>}
+            {title && subtitle && (
+              <div className="title-subtitle-comments">
+                <h1 className="subtitle">{subtitle}</h1>
+                <h3 className="title">{title}</h3>
+              </div>
+            )}
+
+            <div className="image-pat" />
+          </C.CardComments>
+        )}
       </C.ContainerCards>
     </BoxAnimation>
   );
